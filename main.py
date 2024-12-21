f34449fcd19496028ddc75ff8b5bb492f4d9d149
W33nDen <uzshvenko@gmail.com>
2024-12-21 11:54:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,26 @@ from datetime import date, datetime
 
 
 def get_birthdays_per_week(users):
-    # Реалізуйте тут домашнє завдання
+    birthdays = {
+        "monday": [],
+        "tuesday": [],
+        "wednesday": [],
+        "thursday": [],
+        "friday": [],
+    }
+
+    today = datetime.now().date()
+    current_weekday = today.weekday()
+
+    days_until_next_monday = (7 - current_weekday) % 7
+    next_monday = today + days_until_next_monday
+    next_friday = next_monday + datetime.timedelta(days=4)
+
+    for user in users:
+        birthday_this_year = user["birthday"].replace(year=today.year)
+        if next_monday <= birthday_this_year <= next_friday:
+            day_name = birthday_this_year.strftime("%A")
+            birthdays[day_name].append(user["name"])
     return users
 
 
